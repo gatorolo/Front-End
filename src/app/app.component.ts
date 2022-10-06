bca58ff7c9615fc5736437f764a5bf2d044c6a28
gatorolo <rodrigo.daremberg@gmail.com>
2022-10-06 04:00:57 -0300

    20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {faInstagram} from '@fortawesome/free-brands-svg-icons';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faHome} from '@fortawesome/free-solid-svg-icons';
 import {faAddressCard} from '@fortawesome/free-solid-svg-icons';
-import {faHeadSideVirus} from '@fortawesome/free-solid-svg-icons';
+import {faGears} from '@fortawesome/free-solid-svg-icons';
 import {faBuildingColumns} from '@fortawesome/free-solid-svg-icons';
 import {faDiagramProject} from '@fortawesome/free-solid-svg-icons';
 import {faAt} from '@fortawesome/free-solid-svg-icons';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {faUserPen} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -22,11 +23,12 @@ export class AppComponent {
   faInstagram = faInstagram;
   faGithub = faGithub;
   faHome = faHome;
-  faHeadSideVirus = faHeadSideVirus;
+  faGears = faGears;
   faAddressCard = faAddressCard;
   faBuildingColumns = faBuildingColumns;
   faDiagramProject = faDiagramProject;
   faAt = faAt;
   faLinkedinIn = faLinkedinIn;
-  faBars = faBars
+  faBars = faBars;
+  faUserPen = faUserPen
 }
